bd4799cce19da196ef07d88a6dd82b5945dbaac6
Barry Els <barryels@gmail.com>
2023-02-22 20:00:42 +0000

    env var setup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
+dotenvExpand.expand(dotenv.config());
+
 export default defineConfig({
   e2e: {
     specPattern: "**/*.feature",
-    baseUrl: "http://localhost:3000",
     viewportHeight: 614,
     viewportWidth: 360,
     video: false,
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
+    async setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
+      config.baseUrl = process.env.CYPRESS_BASE_URL || "";
+
       await addCucumberPreprocessorPlugin(on, config);
 
       on(
