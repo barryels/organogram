74e3b0e118976dceedd97892a2cf11b2fce710a1
Barry Els <barryels@gmail.com>
2023-02-23 20:30:54 +0000

    add e2e code coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,19 +4,22 @@ import dotenvExpand from "dotenv-expand";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 dotenvExpand.expand(dotenv.config());
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: "cypress/**/*.*",
+    },
+  },
   e2e: {
     specPattern: "**/*.feature",
     viewportHeight: 614,
     viewportWidth: 360,
     video: false,
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
+    async setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): Promise<Cypress.PluginConfigOptions> {
       config.baseUrl = process.env.CYPRESS_BASE_URL || "";
 
       await addCucumberPreprocessorPlugin(on, config);
@@ -27,6 +30,9 @@ export default defineConfig({
           plugins: [createEsbuildPlugin(config)],
         })
       );
+
+      codeCoverageTask(on, config);
+      // require("@cypress/code-coverage/task")(on, config);
 
       return config;
     },
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import istanbul from "vite-plugin-istanbul";
 
 export default ({ mode }: any) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [
+      solidPlugin(),
+      istanbul({
+        include: "src/*",
+        exclude: ["node_modules", "test/"],
+        extension: [".js", ".ts", ".jsx", ".tsx"],
+      }),
+    ],
     base: process.env.VITE_BASE || "/",
     server: {
       port: parseInt(process.env.VITE_SERVER_PORT || "3000"),
